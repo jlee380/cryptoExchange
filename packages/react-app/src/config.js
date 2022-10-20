08a97c37adc37cc119656967c071ae23f9b49dfd
Jonghun Lee <jonghunjhl@gmail.com>
2022-10-19 20:30:13 -0600

    smart contract functions added

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,11 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "";
+export const ROUTER_ADDRESS = "0xb44ebe4C74cfAf8531F19eC846Bffd8c24519D64";
 
 export const DAPP_CONFIG = {
 	readOnlyChainId: Goerli.chainId,
 	readOnlyUrls: {
-		[Goerli.chainId]: "",
+		[Goerli.chainId]:
+			"https://eth-goerli.g.alchemy.com/v2/j0TDHrmoYjBmP_qQjP27ZUTsWoWyDu6-",
 	},
 };
--- packages/react-app/src/utils/index.js
+++ packages/react-app/src/utils/index.js
@@ -1,10 +1,14 @@
-export { 
-  getAvailableTokens,
-  getCounterpartTokens,
-  findPoolByTokens,
-  isOperationPending,
-  getFailureMessage,
-  getSuccessMessage,
-  useAmountsOut,
-  useOnClickOutside
-} from './helpers';
+export { getFactoryInfo } from "getFactoryInfo";
+export { getPairsInfo } from "getPairsInfo";
+export { getRouterInfo } from "getRouterInfo";
+
+export {
+	getAvailableTokens,
+	getCounterpartTokens,
+	findPoolByTokens,
+	isOperationPending,
+	getFailureMessage,
+	getSuccessMessage,
+	useAmountsOut,
+	useOnClickOutside,
+} from "./helpers";
